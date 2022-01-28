ab17637f80cdbac3a8aa3135c34e49d5676d6bf2
guanch92 <guancheng92@gmail.com>
2022-01-28 11:35:54 +0800

    added height styling for navbar

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 const NavigationBar = ( { status, current } ) => {
-  return <div className = "bg-yellow-200">
+  return <div className = "h-max w-screen bg-yellow-200">
   <ul className='flex justify-center'>
 
-    <Link to="/"> <li className="bg-green-300 rounded-lg p-1 text-sm m-2 text-blue-900 hover:font-black">🏡Home</li> </Link> 
-      <Link to="about"> <li className="bg-green-300 rounded-lg p-1 text-sm m-2 text-blue-900 hover:font-black">❓About</li> </Link> 
-      {(current.country === undefined) ? null : (<Link to="regions"> <li className="bg-green-300 rounded-lg p-1 text-sm m-2 text-blue-900 hover:font-black">🌏Globe</li> </Link>)}
+    <Link to="/"> <li className="bg-green-300 rounded-lg p-1 text-sm m-1 text-blue-900 hover:font-black">🏡Home</li> </Link> 
+      <Link to="about"> <li className="bg-green-300 rounded-lg p-1 text-sm m-1 text-blue-900 hover:font-black">❓About</li> </Link> 
+      {(current.country === undefined) ? null : (<Link to="regions"> <li className="bg-green-300 rounded-lg p-1 text-sm m-1 text-blue-900 hover:font-black">🌏Globe</li> </Link>)}
       
   </ul>
   <h4 className='text-xs font-thin text-blue-900'>😊 Data Credits: REST Countries, Unsplash. </h4>
