4484d2fb7d7ee79c4657caaea0e42f29f36888ac
guanch92 <guancheng92@gmail.com>
2022-01-29 11:38:57 +0800

    styled SpaceOddityPage, added background image

--- src/Pages/SpaceOddityPage.jsx
+++ src/Pages/SpaceOddityPage.jsx
@@ -10,28 +10,29 @@ const SpaceOddityPage = () => {
     if (statement === ["Space", "Oddity", "by", "David", "Bowie", "is", "a", "pretty","cool", "song"]) return setIsUnlocked(true);
 
     const statementDisplay = statement.map((e, i) => { 
-        return <span key={i} className="w-28 inline flex-wrap m-1 bg-black border-1 text-white text-2xl border-slate-500 font-bold">{e}</span>    
+        return <span key={i} className="w-28 inline flex-wrap m-1 bg-black border-1 text-white text-3xl border-slate-500 font-bold">{e}</span>    
     } )
 
-  return <div className='flex-inline flex-col flex-nowrap'>
-      <h4 className="m-5 text-2xl font-bold">A Quick Game Before Your <span className='underline'>Out Of This World</span>  Experience. 🚀</h4>  
-      <p className="text-lg">
+  return(
+  <div className="bg-cover h-full bg-right" style={{backgroundImage : `url(${shuttleImage})`}}>
+    <div className='flex-inline flex-col flex-nowrap bg-cyan-100 bg-opacity-75 h-full'>
+      <h4 className="pt-8 pb-4 text-3xl font-bold">A Quick Game Before Your <span className='underline'>Out Of This World</span>  Experience. 🚀</h4>  
+      <p className="text-xl">
         No quizzes on this launchpad, but rather a simple game to get you in the groove. 
         <br/>Click on the <span className='bg-blue-800 text-white font-semibold px-1'>buttons</span> below to guess the statement.
         <br/>Correctly guessing the statement will unlock the button to access the next page. 🧑‍🚀
       </p>
-      {isUnlocked ? <Link to={`/Out%20Of%20This%20World/space`}><button className="m-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">To Infinity & Beyond!</button></Link> : null} 
+      {isUnlocked ? <Link to={`/Out%20Of%20This%20World/space`}><button className="mt-10 mb-2 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">To Infinity & Beyond!</button></Link> : null} 
       <div className='m-2'>
-      <div className='flex mb-4 h-60 aspect-auto justify-center'>
-        <img src={shuttleImage} title="Space Shuttle Take Off" alt="Space Shuttle Take Off" />
-        </div>
-         <div className='flex flex-row flex-wrap justify-center'>
+         <div className='m-8 py-2 flex flex-row flex-wrap justify-center'>
         {statementDisplay}
          </div>
         <SpaceOddityGuessPanel statement={statement} setStatement={setStatement} setIsUnlocked={setIsUnlocked} />
       </div>
-
-  </div>;
+  </div>
+</div>
+    
+  ); 
 };
 
 export default SpaceOddityPage;
