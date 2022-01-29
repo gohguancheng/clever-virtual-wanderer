6d4d734f279a174a4b898f14fcc0b6b71be088e0
guanch92 <guancheng92@gmail.com>
2022-01-29 10:24:45 +0800

    content edit on SpaceOddityPage

--- src/Pages/SpaceOddityPage.jsx
+++ src/Pages/SpaceOddityPage.jsx
@@ -17,13 +17,13 @@ const SpaceOddityPage = () => {
       <h4 className="m-5 text-2xl font-bold">A Quick Game Before Your <span className='underline'>Out Of This World</span>  Experience. 🚀</h4>  
       <p className="text-lg">
         No quizzes on this launchpad, but rather a simple game to get you in the groove. 
-        <br/>Click the below buttons to guess the statement.
-        <br/>Correctly guessing the statement will unlock your button to the next page. 🧑‍🚀
+        <br/>Click on the <span className='bg-blue-800 text-white'>dark blue buttons</span> below to guess the statement.
+        <br/>Correctly guessing the statement will unlock the button to access the next page. 🧑‍🚀
       </p>
       {isUnlocked ? <Link to={`/Out%20Of%20This%20World/space`}><button className="m-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">To Infinity & Beyond!</button></Link> : null} 
       <div className='m-2'>
       <div className='flex mb-4 h-60 aspect-auto justify-center'>
-        <img src={shuttleImage} title="Space Shuttle Take Off" />
+        <img src={shuttleImage} title="Space Shuttle Take Off" alt="Space Shuttle Take Off" />
         </div>
          <div className='flex flex-row flex-wrap justify-center'>
         {statementDisplay}
